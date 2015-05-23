70baf5e5f946ab955bdffecd4fcaa21e9b128f0d
Amaury <phyzalis@yahoo.com>
2015-05-23 12:00:26 +0200

    Added belongs to

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ActiveRecord::Base
+  belongs_to :platform
+
+  validates :name, :platform, presence: true
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,2 +1,5 @@
 class Platform < ActiveRecord::Base
+  has_many :games
+
+  validates :name, presence: true
 end
